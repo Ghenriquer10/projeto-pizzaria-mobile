e3635083c1730684a461a8e7657c08f365f4d5ff
Gustavo Henrique <ghenriquer10@gmail.com>
2022-08-03 16:25:44 -0300

    Implementando logica para adquirir os dados dos inputs

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     View,
     Text,
@@ -9,6 +9,18 @@ import {
 } from 'react-native';
 
 export default function SignIn() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleLogin() {
+        if (email === '' || password === '') {
+            alert('Preencha os campos!')
+            return
+        }
+        console.log('Email digitado: ' + email)
+    }
+
     return (
         <View style={styles.container}>
             <Image style={styles.logo} source={require('../../assets/logo.png')} />
@@ -17,14 +29,18 @@ export default function SignIn() {
                     placeholder={'Digite seu email'}
                     style={styles.input}
                     placeholderTextColor={'#fff'}
+                    value={email}
+                    onChangeText={setEmail}
                 />
                 <TextInput
                     placeholder={'Digite sua senha'}
                     style={styles.input}
                     placeholderTextColor={'#fff'}
                     secureTextEntry={true}
+                    value={password}
+                    onChangeText={setPassword}
                 />
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity style={styles.button} onPress={handleLogin}>
                     <Text style={styles.buttonText}>Acessar</Text>
                 </TouchableOpacity>
             </View>
