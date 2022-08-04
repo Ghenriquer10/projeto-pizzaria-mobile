1f4ffe276c20f1c7b0c2a8ff9cc8c5dff365c9fa
Gustavo Henrique <ghenriquer10@gmail.com>
2022-08-04 17:16:01 -0300

    Recebendo parametros na tela 'Order'

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,11 +1,25 @@
-import { useNavigation } from '@react-navigation/native';
+import { useRoute, RouteProp } from '@react-navigation/native';
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native';
 
+type RouteDetailParams = {
+    Order: {
+        table: number | string;
+        order_id: string;
+    }
+}
+
+type OrderRouteProps = RouteProp<RouteDetailParams, 'Order'>
+
 export default function Order() {
+
+    const route = useRoute<OrderRouteProps>();
+
     return (
         <View style={styles.container}>
             <Text>Página Order</Text>
+            <Text>{route.params.table}</Text>
+            <Text>{route.params.order_id}</Text>
         </View>
     )
 }
