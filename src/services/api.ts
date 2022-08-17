dfcea664f1b56fe946cbcbfc327c5739c554849d
Gustavo Henrique <ghenriquer10@gmail.com>
2022-08-17 20:50:17 -0300

    atualizando json do aplicativo

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://192.168.0.186:3333'
+    baseURL: 'https://projeto-pizzaria-backend.herokuapp.com'
 })
 
 export { api }
